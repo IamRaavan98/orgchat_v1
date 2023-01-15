90f40074ad5e2cf1c2fb1a377cfb2406e9894827
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2023-01-15 17:21:50 +0530

    cors creadiantials removed

--- server/app.js
+++ server/app.js
@@ -14,8 +14,7 @@ const DBconnection = require("./config/DB")
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }));
     app.use(cors({
-    origin: 'https://orgchat-v1.vercel.app/',
-    credentials: true,
+    origin: 'https://orgchat-v1.vercel.app',
 }));
     DBconnection();
     app.use("/",user)
