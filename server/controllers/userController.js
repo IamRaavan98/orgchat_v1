ee9023df7cb3b525f91d63275e2c6fdb766d57f4
Rohan Agrawal <52717036+IamRaavan98@users.noreply.github.com>
2023-01-15 21:25:40 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,11 +23,11 @@ exports.login = async (req, res) => {
       // if you want to use cookies
       const options = {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        domain:"https://orgchat-v1.vercel.app",
         httpOnly: true,
       };
 
       res.status(200).cookie("token", token, options).json({
-
         success: true,
         token,
         user,
