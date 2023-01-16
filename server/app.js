9bdf4dc6781d9f6d6e7223e8011121eb683513a5
Rohan Agrawal <52717036+IamRaavan98@users.noreply.github.com>
2023-01-16 11:53:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const DBconnection = require("./config/DB")
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }));
     app.use(cors({
-      origin:"http://192.168.56.1:3000",
+      origin:"https://localhost:3000",
     }));
     DBconnection();
     app.use("/",user)
